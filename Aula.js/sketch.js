c7b802edc35e9fcc598214495c4871102d7e5a62
Silas-Sales <164197231+Silas-Sales@users.noreply.github.com>
2024-04-08 11:30:10 -0300

    Add files via upload

---
+++ Aula.js/sketch.js
@@ -0,0 +1,17 @@
+// window.alert('Meu primeiro alerta')//////cria alerta
+// window.confirm('Estar gostando do curso')////comfirmação
+// window.prompt('Qual seu nome')////caixa de pergunta
+// var nome = Window.prompt('Qual seu nome?')
+// window.alert('Seja bem vindo' + nome +'!' )
+
+//criando 2 numeros
+// var n1 = Number.parseFloat(window.prompt('Digite um numero'))
+// var n2 = Number.parseFloat(window.prompt('Diite outro numero'))
+// var s = n1+n2
+// var s = window.alert('A soma dois 2 numeros são'+ s)
+
+//Criando numero flutuante
+var n1 = Number(window.prompt('Digite um numero'))
+var n2 = Number(window.prompt('Digite outro numero'))
+var s = n1 + n2
+var s = window.alert('A soma dos numeros são ' + s)
